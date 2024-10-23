3a48bb52fef79571a30f2d5fe22cb07ec7d56b0c
M Alexander Diaz <m_alexander_d@outlook.es>
2024-10-22 23:36:26 -0500

    These controllers are examples of how you can secure endpoints based on roles

--- src/main/java/com/alexvoneinzbern/jwt_demo/controllers/AdminController.java
+++ src/main/java/com/alexvoneinzbern/jwt_demo/controllers/AdminController.java
@@ -1,4 +1,19 @@
 package com.alexvoneinzbern.jwt_demo.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/admins")
 public class AdminController {
+
+  @GetMapping("/admin")
+  @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+  public String Admin(Principal principal) {
+    return "Hello, " + principal.getName();
+  }
 }
--- src/main/java/com/alexvoneinzbern/jwt_demo/controllers/ClientController.java
+++ src/main/java/com/alexvoneinzbern/jwt_demo/controllers/ClientController.java
@@ -1,4 +1,19 @@
 package com.alexvoneinzbern.jwt_demo.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/clients")
 public class ClientController {
+
+  @GetMapping("/client")
+  @PreAuthorize("hasAuthority('ROLE_USER')")
+  public String Client(Principal principal) {
+    return "Hello, " + principal.getName();
+  }
 }
